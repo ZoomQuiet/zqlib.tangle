3f69763aa3f10f1b1d6a644f2e685a6483aedc6e
Zoom Quiet <Zoom.Quiet@gmail.com>
2009-11-29 16:46:56 +0800

    ???????? for obp.zoomquiet.org

---
+++ tasks/py.k/_cron/zoomq-cron.sh
@@ -0,0 +1,11 @@
+# crontab - zoomq's crontab for Zues
+#
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+HOME=/home/zoomq/zoomquiet.org
+MAILTO=Zoom.Quiet@gmail.com
+#
+#minute	hour	mday	month	wday	command
+*/5   *   *   *   * /home/zoomq/zoomquiet.org/_cron/obp-shpinx-up.sh
+
+
