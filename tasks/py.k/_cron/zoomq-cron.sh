35260a467dba93b0cdb9f180f009407c67628093
Zoom Quiet <Zoom.Quiet@gmail.com>
2009-11-29 18:33:53 +0800

    ??obp.zoomquiet.org ????????

--- tasks/py.k/_cron/zoomq-cron.sh
+++ tasks/py.k/_cron/zoomq-cron.sh
@@ -8,6 +8,6 @@ MAILTO=Zoom.Quiet@gmail.com
 #minute	hour	mday	month	wday	command
 ###zoomq:091129 paaended for obp.zoomquiet auto build Sphinx+.dot
 */15   *   *   *   * /home/zoomq/zoomquiet.org/_cron/obp-shpinx-up.sh
-*/2   *   *   *   * /home/zoomq/zoomquiet.org/_cron/obp-idx-up.sh
+*   */8   *   *   * /home/zoomq/zoomquiet.org/_cron/obp-idx-up.sh
 
 
