a95356ceba805c906a3845e67faa111c95507956
Zoom Quiet <Zoom.Quiet@gmail.com>
2010-03-15 15:28:04 +0800

    ???? MTS ??????

---
+++ tangle/mts2avi2ogv/zss1-mencoder-avi.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#mencoder 02458.avi -o 02458.ogv -ovc lavc -oac mp3lame
+#mencoder 02459.avi -o 02459.ogv -ovc lavc -oac mp3lame
+mencoder 02460.avi -o 02460.ogv -ovc lavc -oac mp3lame
+mencoder 02461.avi -o 02461.ogv -ovc lavc -oac mp3lame
+mencoder 02462.avi -o 02462.ogv -ovc lavc -oac mp3lame
+mencoder 02463.avi -o 02463.ogv -ovc lavc -oac mp3lame
+mencoder 02464.avi -o 02464.ogv -ovc lavc -oac mp3lame
+mencoder 02465.avi -o 02465.ogv -ovc lavc -oac mp3lame
+mencoder 02466.avi -o 02466.ogv -ovc lavc -oac mp3lame
+mencoder 02467.avi -o 02467.ogv -ovc lavc -oac mp3lame
+mencoder 02468.avi -o 02468.ogv -ovc lavc -oac mp3lame
+mencoder 02469.avi -o 02469.ogv -ovc lavc -oac mp3lame
+mencoder 02470.avi -o 02470.ogv -ovc lavc -oac mp3lame
+mencoder 02471.avi -o 02471.ogv -ovc lavc -oac mp3lame
+mencoder 02472.avi -o 02472.ogv -ovc lavc -oac mp3lame
+mencoder 02473.avi -o 02473.ogv -ovc lavc -oac mp3lame
+mencoder 02474.avi -o 02474.ogv -ovc lavc -oac mp3lame
+
+echo "all avi->ogv by mencoder!"
+
